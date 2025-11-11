2a7c42247d062d26b95ae20bbbac23f04a887116
snawiti <snawiti@yahoo.com>
2025-11-11 16:14:40 +0200

    Setting up project directories

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to alx-project-0x00</h1>
-    </div>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
   )
 }
+
+export default Home;
